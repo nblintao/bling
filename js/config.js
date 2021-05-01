28b3e0f7834c5705cd808d58f52e06e922979a6c
Tao Lin <nblintao@gmail.com>
2021-05-01 16:44:07 -0700

    user pooll

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_nrJByhqaX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5q1oc0s2smmr1bj9u3or9p04aq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
